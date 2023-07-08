c87ce1a0154873c4a6b90ba2acc61742af150fe0
Malvepooja1704 <malvepooja303@gmail.com>
2023-07-08 19:41:11 +0530

    Login button

---
+++ satvachintan/src/components/LOgin.js
@@ -0,0 +1,22 @@
+ import React from "react"
+  
+ const Login = () =>{
+    return (
+        <div>
+            <h1>Login</h1>
+            <div>
+            <input type="text" placeholder="Enter User name"></input>
+            <br></br>
+            <input type="password" placeholder="Enter Password"></input>
+            </div>
+            <div>
+            <button type="button">Login</button>
+            </div>
+        </div>
+    )
+
+
+
+ } 
+ export default Login;
+
